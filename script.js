4cca5d3345ae5229ca66243f8980b8fff5782826
ABR <adrianabonilla-ramirez@MacBook-Air.local>
2020-10-05 17:54:13 -0400

    added if statement for past, present, future

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-$(document).ready(function(){
-    console.log("this works somehow");
+$(document).ready(function () {
+  console.log("this works somehow");
 
-    moment(Date);
-    $("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+  moment(Date);
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
 
-//DOM VARIABLES
+  //DOM VARIABLES
 
-var currentTime = moment();
-currentTime = currentTime.startOf("hour");
+  var currentTime = moment();
+  currentTime = currentTime.hours();
 
-var x2 = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+  var timeOfday = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 
-x2.forEach(function (time) {
-
+  timeOfday.forEach(function (time) {
     var newTime = [];
-    newTime.push(moment().hour(time).minutes("00").format('hh:mm A'));
+    newTime.push(moment().hour(time).minutes("00").format("hh:mm A"));
 
     // AddING Elements
     var newDiv = $("<div>");
@@ -23,17 +22,17 @@ x2.forEach(function (time) {
     var addSpan = $("<span>");
     addSpan.addClass("input-group-text time-block block");
 
-    var inputText = $('<input>').attr('type', 'text');
-    inputText.addClass("form-control form");
-    inputText.data("Hour");
+    var inputText = $("<input>").attr("type", "text");
+    inputText.addClass(`form-control form ${time}`);
+    inputText.data("hour");
 
-    var inputSubmit = $('<input>').attr('type', 'submit');
+    var inputSubmit = $("<input>").attr("type", "submit");
     inputSubmit.addClass("btn saveBtn");
 
     var icon = $("<i>");
     icon.addClass("far fa-save");
 
-    // appends here: 
+    // appends here:
     $(".container").append(newDiv);
     newDiv.append(addSpan);
     newDiv.append(inputText);
@@ -41,39 +40,59 @@ x2.forEach(function (time) {
     inputSubmit.append(icon);
 
     addSpan.text(newTime);
-    // If/Else Statement
-    if (currentTime.isAfter(newTime)) {
-        $(".form").addClass("past");
-    }
-    else if (currentTime.isBefore(newTime)) {
-        $(".form").addClass("future");
-    }
-    else if (currentTime.isSame(newTime)) {
-        $(".form").addClass("present");
-    };
 
+    if (time < currentTime) {
+      inputText.addClass("past");
+    } else if (time === currentTime) {
+      inputText.removeClass("past");
+      inputText.addClass("present");
+    } else {
+      inputText.removeClass("past");
+      inputText.removeClass("present");
+      inputText.addClass("future");
+    }
+
+    // $(".saveBtn").click(function (event) {
+    //   event.preventDefault();
+    //   var formValue = $(this).siblings(".form-control").val();
+    //   console.log(formValue );
+    //   var listItem = $(this).siblings("input").attr("class").split(" ")[2];
+
+    //   localStorage.setItem(listItem, formValue);
+    // });
+  });
+
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var formValue = $(this).siblings(".form-control").val();
+
+    var listItem = $(this).siblings("input").attr("class").split(" ")[2];
+    
+    localStorage.setItem(listItem, formValue);
+  });
+
+  //Initial Functions
+  $(".9").val(localStorage.getItem("9"));
+  $(".10").val(localStorage.getItem("10"));
 });
 
-// Start local
-var x = [9, 10, 11, 12, 1, 2, 3, 4, 5];
-// Test loop:
+// // Start local
+// var x = [9, 10, 11, 12, 1, 2, 3, 4, 5];
+// // Test loop:
 
-for (var i = 0; i < x.length; i++) {
-    var dataHour = localStorage.getItem(x[i]);
-    // form - control
-    $(".form" + x[i]).val(dataHour);
-}
+// for (var i = 0; i < x.length; i++) {
+//     var dataHour = localStorage.getItem(x[i]);
+//     // form - control
+//     $(".form" + x[i]).val(dataHour);
+// }
 
 // EVENT LISTENERS
 
-$(".saveBtn").click(function () {
-    event.preventDefault();
-    var formValue = $(this).siblings(".form-control").val();
-    console.log("This worked");
-    var listItem = $(this).parent().data("hour");
+// $(".saveBtn").click(function () {
+//     event.preventDefault();
+//     var formValue = $(this).siblings(".form-control").val();
+//     console.log("This worked");
+//     var listItem = $(this).parent().data("hour");
 
-    localStorage.setItem(listItem, formValue);
-});
-
-
-});
+//     localStorage.setItem(listItem, formValue);
+// });
